7c007359c159e64e53d061a42a1663f72e1e697d
mauvaisetroupe <lionel.brol@gmail.com>
2026-08-30 19:29:10 +0200

    change  exercices order

--- frontend/src/exercises.ts
+++ frontend/src/exercises.ts
@@ -5,7 +5,7 @@ export interface Exercise {
 
 export const exercises: Exercise[] = [
   { name: 'Inverted row', icon: '↓' },  // pull
-  { name: 'Push-ups', icon: '↑' },      // push
   { name: 'Biceps curl', icon: '↓' },   // pull
-  { name: 'Support Hold', icon: '⏺' }    // isometric
+  { name: 'Support Hold', icon: '⏺' },   // isometric
+  { name: 'Push-ups', icon: '↑' },      // push
 ]
