712718cb0fddbe291687246bce8633a1c42c85bc
mauvaisetroupe <lionel.brol@gmail.com>
2026-09-02 21:22:14 +0200

    PWA ready

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,48 @@
 import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        id: '/',
+        name: 'CountRep',
+        short_name: 'CountRep',
+        description: 'Track your daily repetitions',
+
+        theme_color: '#fdf8f5',
+        background_color: '#fdf8f5',
+
+        display: 'standalone',
+        start_url: '/',
+        scope: '/',
+
+        icons: [
+          {
+            src: '/launchericon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/launchericon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          }
+        ]
+
+      }
+    })
+  ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
